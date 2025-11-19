07e7c00076d46aab04de990b1d4407848e7d7c60
Michael Montemayor <montemayorm@msoe.edu>
2025-11-18 23:28:45 -0600

    added processor control to the command line

--- mandel.c
+++ mandel.c
@@ -13,7 +13,6 @@
 #include <wait.h>
 #include <math.h>
 #include <string.h>
-#define MAX_PROC 20
 
 // local routines
 static int iteration_to_color( int i, int max );
@@ -25,60 +24,62 @@ static void show_help();
 
 int main( int argc, char *argv[] )
 {
-	char c;
-    int active_proc = 0;
+  char c;
+  int active_proc = 0;
 
-	// These are the default configuration values used
-	// if no command line arguments are given.
-	char outfile[256] = "mandel";
-	double xcenter = 0;
-	double ycenter = 0;
-	double xscale = 4;
-	int    image_width = 1000;
-	int    image_height = 1000;
-	int    max = 1000;
+  // These are the default configuration values used
+  // if no command line arguments are given.
+  char outfile[256] = "mandel";
+  double xcenter = 0;
+  double ycenter = 0;
+  double xscale = 4;
+  int    image_width = 1000;
+  int    image_height = 1000;
+  int    max = 1000;
   int    num = 1;
+  int    max_proc = 20; 
+  // For each command line argument given,
+  // override the appropriate configuration value.
 
-	// For each command line argument given,
-	// override the appropriate configuration value.
-
-	while((c = getopt(argc,argv,"x:y:s:W:H:m:o:n:h"))!=-1) {
-		switch(c) 
-		{
-			case 'x':
-				xcenter = atof(optarg);
-				break;
-			case 'y':
-				ycenter = atof(optarg);
-				break;
-			case 's':
-				xscale = atof(optarg);
-				break;
-			case 'W':
-				image_width = atoi(optarg);
-				break;
-			case 'H':
-				image_height = atoi(optarg);
-				break;
-			case 'm':
-				max = atoi(optarg);
-				break;
-			case 'o':
-				strncpy(outfile, optarg, sizeof(outfile)-1);
-    		outfile[sizeof(outfile)-1] = '\0';
-				break;
+  while((c = getopt(argc,argv,"x:y:s:W:H:m:o:n:p:h"))!=-1) {
+    switch(c) 
+	{
+	  case 'x':
+		xcenter = atof(optarg);
+		break;
+	  case 'y':
+		ycenter = atof(optarg);
+		break;
+	  case 's':
+		xscale = atof(optarg);
+		break;
+	  case 'W':
+		image_width = atoi(optarg);
+		break;
+	  case 'H':
+		image_height = atoi(optarg);
+		break;
+	  case 'm':
+		max = atoi(optarg);
+		break;
+	  case 'o':
+		strncpy(outfile, optarg, sizeof(outfile)-1);
+        outfile[sizeof(outfile)-1] = '\0';
+		break;
       case 'n':
         num = atoi(optarg);
         break;
-			case 'h':
-				show_help();
-				exit(1);
-				break;
-		}
+      case 'p':
+        max_proc = atoi(optarg);
+	  case 'h':
+		show_help();
+		exit(1);
+		break;
 	}
+  }
 
     for(int i = 0; i < num; i++){
-        if(active_proc >= MAX_PROC){
+        if(active_proc >= max_proc){
             wait(NULL);
             active_proc--;
         }
@@ -197,19 +198,20 @@ int iteration_to_color( int iters, int max )
 // Show help message
 void show_help()
 {
-	printf("Use: mandel [options]\n");
-	printf("Where options are:\n");
-	printf("-m <max>    The maximum number of iterations per point. (default=1000)\n");
-	printf("-x <coord>  X coordinate of image center point. (default=0)\n");
-	printf("-y <coord>  Y coordinate of image center point. (default=0)\n");
-	printf("-s <scale>  Scale of the image in Mandlebrot coordinates (X-axis). (default=4)\n");
-	printf("-W <pixels> Width of the image in pixels. (default=1000)\n");
-	printf("-H <pixels> Height of the image in pixels. (default=1000)\n");
-	printf("-o <file>   Set output file. (default=mandel.bmp)\n");
+  printf("Use: mandel [options]\n");
+  printf("Where options are:\n");
+  printf("-m <max>    The maximum number of iterations per point. (default=1000)\n");
+  printf("-x <coord>  X coordinate of image center point. (default=0)\n");
+  printf("-y <coord>  Y coordinate of image center point. (default=0)\n");
+  printf("-s <scale>  Scale of the image in Mandlebrot coordinates (X-axis). (default=4)\n");
+  printf("-W <pixels> Width of the image in pixels. (default=1000)\n");
+  printf("-H <pixels> Height of the image in pixels. (default=1000)\n");
+  printf("-o <file>   Set output file. (default=mandel.bmp)\n");
+  printf("-p <process>Set the number of processes to use\n");
   printf("-n <number> The number of files to create\n");
-	printf("-h          Show this help text.\n");
-	printf("\nSome examples are:\n");
-	printf("mandel -x -0.5 -y -0.5 -s 0.2\n");
-	printf("mandel -x -.38 -y -.665 -s .05 -m 100\n");
-	printf("mandel -x 0.286932 -y 0.014287 -s .0005 -m 1000\n\n");
+  printf("-h          Show this help text.\n");
+  printf("\nSome examples are:\n");
+  printf("mandel -x -0.5 -y -0.5 -s 0.2\n");
+  printf("mandel -x -.38 -y -.665 -s .05 -m 100\n");
+  printf("mandel -x 0.286932 -y 0.014287 -s .0005 -m 1000\n\n");
 }
